588aa0ef09d3b6cda4b09ecc2620a7d3d2e08da1
Ravi Teja Gudapati <tejainece@gmail.com>
2017-12-28 05:56:59 +0100

    PasswordUser

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.2
+
++ Added `PasswordUser`
+
 ## 0.0.1
 
 - Initial version, created by Stagehand
--- lib/src/user_model.dart
+++ lib/src/user_model.dart
@@ -8,40 +8,40 @@ abstract class AuthorizationUser {
   String get authorizationId;
 }
 
+/// Interface for a user model with a password
+abstract class PasswordUser implements AuthorizationUser {
+  /// Secret password the user uses to authenticate.
+  String get password;
+}
+
 /// [AuthorizationUser] that uses email as authenticationId and unique record id as
 /// authorizationId
-abstract class UserEmail implements AuthorizationUser {
+abstract class UserEmail implements PasswordUser {
   String get id;
 
   String get email;
-
-  String get password;
 
   String get authorizationId => id;
 }
 
 /// [AuthorizationUser] that uses username as authenticationId and unique record id as
 /// authorizationId
-abstract class UserUsername implements AuthorizationUser {
+abstract class UserUsername implements PasswordUser {
   String get id;
 
   String get username;
-
-  String get password;
 
   String get authorizationId => id;
 }
 
 /// [AuthorizationUser] that uses username and email as authenticationId and unique
 /// record id as authorizationId
-abstract class UserUsernameEmail implements AuthorizationUser {
+abstract class UserUsernameEmail implements PasswordUser {
   String get id;
 
   String get username;
 
   String get email;
-
-  String get password;
 
   String get authorizationId => id;
 }
